5ceb43d6d041fb21751d820018aa19481460c645
Matthew Wilding <mbwilding@gmail.com>
2025-04-28 23:48:05 +0800

    2025-04-28-23-48-05

--- lua/launcher/modules/unreal_engine.lua
+++ lua/launcher/modules/unreal_engine.lua
@@ -13,7 +13,7 @@ M.definitions = {
             lsp = function(opts)
                 -- TODO: Find path or get consumer to set a global var with path
                 local engine_path = "~/dev/UnrealEngine/Engine/Build/BatchFiles/Linux/Build.sh"
-                return '"'
+                return 'sh "'
                     .. engine_path
                     .. '" '
                     .. ' -mode=GenerateClangDatabase -project="'
