734547b41973f79b2d9f6ee5d1d8df4c4163ffaf
Matthew Wilding <mbwilding@gmail.com>
2025-04-28 23:56:56 +0800

    2025-04-28-23-56-56

--- lua/launcher/modules/unreal_engine.lua
+++ lua/launcher/modules/unreal_engine.lua
@@ -12,18 +12,17 @@ M.definitions = {
         commands = {
             lsp = function(opts)
                 -- TODO: Find path or get consumer to set a global var with path
-                local engine_path = "~/dev/UnrealEngine/Engine/Build/BatchFiles/Linux/Build.sh"
-                return 'set -eax && ./"'
-                    .. engine_path
-                    .. '" '
-                    .. '-mode=GenerateClangDatabase -project="'
-                    .. opts.file_path_absolute
-                    .. '" -game -engine HexEditor Linux Development'
-                    -- .. ' && cp "'
-                    -- .. engine_path
-                    -- .. '/compile_commands.json" "'
-                    -- .. opts.file_path_absolute
-                    -- .. '"'
+                local home = os.getenv("HOME")
+                local engine_path = home .. "/dev/UnrealEngine/Engine/Build/BatchFiles/Linux/Build.sh"
+                local cmd = 'set -eax && "' .. engine_path .. '" ' ..
+                    '-mode=GenerateClangDatabase -project="' ..
+                    opts.file_path_absolute .. '" -game -engine HexEditor Linux Development'
+                return cmd
+                -- .. ' && cp "'
+                -- .. engine_path
+                -- .. '/compile_commands.json" "'
+                -- .. opts.file_path_absolute
+                -- .. '"'
             end,
         },
     },
