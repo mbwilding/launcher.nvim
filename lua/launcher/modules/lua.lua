9cd129e60122e20010508525ffed91a12ff2bda1
Matthew Wilding <mbwilding@gmail.com>
2025-05-13 19:56:30 +0800

    2025-05-13-19-56-30

--- lua/launcher/modules/lua.lua
+++ lua/launcher/modules/lua.lua
@@ -4,11 +4,11 @@ local exe = ft
 
 ---@type Launcher.Module
 local M = {
+    required_exe = exe,
     definitions = {
         {
             icon = icon,
             ft = ft,
-            required_exe = exe,
             commands = {
                 run = function(file)
                     return exe .. " " .. file.path_absolute_dq
